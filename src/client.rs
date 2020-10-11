73cd8cb293a0c94a6bdda9e89e8da09d5a670bb2
Lee Tae Woo <syr@meu.works>
2020-10-11 11:10:50 +0900

    fix url bug

--- src/client.rs
+++ src/client.rs
@@ -1,50 +1,45 @@
 use reqwest;
 
-use super::TokenManager;
-
 pub struct Client {
     base_url: String,
     request: reqwest::Client,
-
-    pub token_manager: TokenManager,
 }
 
 impl Client {
-    pub fn new(base_url: &str, token_manager: TokenManager) -> Self {
+    pub fn new(base_url: &str) -> Self {
         Self {
             base_url: base_url.to_string(),
-            token_manager,
             request: reqwest::Client::builder().build().unwrap(),
         }
     }
 
     pub fn get(&self, path: &str) -> reqwest::RequestBuilder {
         self.request
-            .get(format!("{}/{}", self.base_url, path).as_str())
+            .get(format!("{}{}", self.base_url, path).as_str())
     }
 
     pub fn post(&self, path: &str) -> reqwest::RequestBuilder {
         self.request
-            .post(format!("{}/{}", self.base_url, path).as_str())
+            .post(format!("{}{}", self.base_url, path).as_str())
     }
 
     pub fn delete(&self, path: &str) -> reqwest::RequestBuilder {
         self.request
-            .delete(format!("{}/{}", self.base_url, path).as_str())
+            .delete(format!("{}{}", self.base_url, path).as_str())
     }
 
     pub fn patch(&self, path: &str) -> reqwest::RequestBuilder {
         self.request
-            .patch(format!("{}/{}", self.base_url, path).as_str())
+            .patch(format!("{}{}", self.base_url, path).as_str())
     }
 
     pub fn put(&self, path: &str) -> reqwest::RequestBuilder {
         self.request
-            .put(format!("{}/{}", self.base_url, path).as_str())
+            .put(format!("{}{}", self.base_url, path).as_str())
     }
 
     pub fn head(&self, path: &str) -> reqwest::RequestBuilder {
         self.request
-            .head(format!("{}/{}", self.base_url, path).as_str())
+            .head(format!("{}{}", self.base_url, path).as_str())
     }
 }
