13bcb609e75b834aa838d2acbb7bc5941eafadc1
Lee Tae Woo <syr@meu.works>
2020-10-11 11:13:50 +0900

    add response_error()

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,24 @@ pub mod book;
 pub mod file;
 
 mod client;
-mod token_manager;
 
 pub use client::Client;
-pub use token_manager::TokenManager;
 
 pub use auth::AuthClient;
 pub use book::BookClient;
 pub use file::FileClient;
+
+pub fn response_error(
+    error: reqwest::Error,
+    method: &str,
+    url: &str,
+    data: String,
+) -> anyhow::Error {
+    anyhow::Error::msg(format!(
+        "{} {}\n{}\n{}",
+        method,
+        url,
+        error.status().unwrap(),
+        data
+    ))
+}
