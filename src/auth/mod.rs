b5d4e0e62fe23044a29ebfe1a2ee9926d7e218fa
Lee Tae Woo <syr@meu.works>
2020-10-11 11:11:36 +0900

    Content-Type header

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -2,41 +2,68 @@ mod models;
 
 pub use models::{Token, TokenState};
 
-use super::Client;
+use super::{response_error, Client};
 
-pub struct AuthClient<'a> {
-    pub client: &'a Client,
+pub struct AuthClient {
+    client: Client,
 }
 
-impl<'a> AuthClient<'a> {
-    pub fn new(client: &'a Client) -> Self {
-        Self { client }
+impl AuthClient {
+    pub fn new(base_url: &str) -> Self {
+        Self {
+            client: Client::new(base_url),
+        }
     }
 
-    pub async fn valid_token(&self) -> anyhow::Result<bool> {
+    pub async fn valid_token(&self, token: &String) -> anyhow::Result<()> {
         let response = self
             .client
             .get("/v1/auth/token")
-            .header("Authorization", self.client.token_manager.token())
+            .header("Authorization", token)
             .send()
             .await?;
 
-        let TokenState { enable } = response.json::<TokenState>().await?;
+        match response.error_for_status_ref() {
+            Ok(_) => {
+                /* let TokenState { enable } = response.json::<TokenState>().await?;
 
-        Ok(enable)
+                Ok(enable) */
+                Ok(())
+            }
+            Err(err) => Err(response_error(
+                err,
+                "POST",
+                "/v1/auth/token",
+                response.text().await?,
+            )),
+        }
     }
 
-    pub async fn refresh_token(&self) -> anyhow::Result<String> {
-        let response = self
+    pub async fn refresh_token(&self, token: &String) -> anyhow::Result<String> {
+        let request = self
             .client
             .post("/v1/auth/token")
-            .header("Authorization", self.client.token_manager.token())
-            .body("{\"type\": \"refresh\"}")
-            .send()
-            .await?;
+            .header("Authorization", token)
+            .header("Content-Type", "application/json")
+            .body("{\"type\": \"refresh\"}");
 
-        let Token { token: new_token } = response.json::<Token>().await?;
+        println!("{:?}", request);
 
-        Ok(new_token)
+        let response = request.send().await?;
+        println!("{:?}", &response);
+
+        match response.error_for_status_ref() {
+            Ok(_) => {
+                let Token { token: new_token } = response.json().await?;
+
+                Ok(new_token)
+            }
+            Err(err) => Err(response_error(
+                err,
+                "POST",
+                "/v1/auth/token",
+                response.text().await?,
+            )),
+        }
     }
 }
